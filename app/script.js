16c8d53f846b3ed45932dcbdd8b7af390855b287
Alchium <133685267+Alchium@users.noreply.github.com>
2023-05-30 18:07:35 -0300

    User querySelectors

--- app/script.js
+++ app/script.js
@@ -8,23 +8,31 @@ const KEY = `b725d7c3f1302cf77740c3198f72ff35`;
 // Variables //
 
 // Usernames
-let USER1 = `Superfrancis123`;
-let USER2 = `Exticx`;
+// let USER1 = `Superfrancis123`;
+const USER1 = document.querySelector('.username__input__self');
+const USER2 = document.querySelector('.username__input__friend');
 
 //// USER 1 DATA ////
 // Calls data for the first user from the Last.fm API and stores it on the site
 
+let U1_TopArtists;
+let U1_TopTracks;
+let U1_RecentTracks;
+let U1_UserInfo;
+
 // Top Stats //
 
-// Top Artists 
+// Top Artists
+
 fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
   .then(response => response.json())
   .then(data => {
-    const U1topArtists = data.topartists.artist;
-
-    console.log(U1topArtists)
-
+    U1_TopArtists = data.topartists.artist;
+    console.log("Fetched Data:", U1_TopArtists)
   })
+
+
+console.log("Stored data:", U1_TopArtists)
 
 // Top Tracks
 fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
@@ -32,7 +40,7 @@ fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&limit=5&user=$
   .then(data => {
     const U1topTracks = data.toptracks.track;
 
-    console.log(U1topTracks)
+    // console.log(U1topTracks)
 
   })
 
@@ -44,7 +52,7 @@ fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&limit=5&use
   .then(data => {
     const U1recentTracks = data.recenttracks.track;
 
-    console.log(U1recentTracks)
+    // console.log(U1recentTracks)
 
   })
 
@@ -56,12 +64,17 @@ fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getInfo&limit=5&user=${USER
   .then(data => {
     const U1userInfo = data.user;
 
-    console.log(U1userInfo)
+    // console.log(U1userInfo)
 
   })
 
 //// USER 2 DATA ////
 // Calls data for the second user from the Last.fm API and stores it on the site
+
+let U2_TopArtists;
+let U2_TopTracks;
+let U2_RecentTracks;
+let U2_UserInfo;
 
 // Top Stats //
 
@@ -71,7 +84,7 @@ fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&limit=5&user=
   .then(data => {
     const U2topArtists = data.topartists.artist;
 
-    console.log(U2topArtists)
+    // console.log(U2topArtists)
 
   })
 
@@ -81,7 +94,7 @@ fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&limit=5&user=$
   .then(data => {
     const U2topTracks = data.toptracks.track;
 
-    console.log(U2topTracks)
+    // console.log(U2topTracks)
 
   })
 
@@ -93,19 +106,19 @@ fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&limit=5&use
   .then(data => {
     const U2recentTracks = data.recenttracks.track;
 
-    console.log(U2recentTracks)
+    // console.log(U2recentTracks)
 
   })
 
 // Overall Stats //
 
 // User Info
-fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getInfo&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
+fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getInfo&limit=5&user=${USER2}&api_key=${KEY}&format=json`)
   .then(response => response.json())
   .then(data => {
     const U2userInfo = data.user;
 
-    console.log(U2userInfo)
+    // console.log(U2userInfo)
 
   })
 
@@ -113,3 +126,5 @@ fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getInfo&limit=5&user=${USER
 // Specifically for leaderboards, gathers locally stored data and calculates it according to what we want to display in the leaderboards.
 
 // Total Scrobble Difference
+
+console.log(U1_TopArtists)
