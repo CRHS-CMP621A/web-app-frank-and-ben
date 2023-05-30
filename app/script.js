a0c4f112fb28a54afe85f8c66aaf8aeac9878e19
Alchium <133685267+Alchium@users.noreply.github.com>
2023-05-30 19:19:41 -0300

    button test

--- app/script.js
+++ app/script.js
@@ -24,7 +24,7 @@ fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&limit=5&user=
   console.log("Fetched Data:", U1_TopArtists)
 })
 
-form.addEventListener("submit", function(e) {
+button.addEventListener("click", function(e) {
   e.preventDefault()
   
   console.log("Stored data:", U1_TopArtists)
