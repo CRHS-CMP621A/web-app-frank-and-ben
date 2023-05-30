2d648746d47db1c42b6d47b6bc7b6a7f680c89b2
Alchium <133685267+Alchium@users.noreply.github.com>
2023-05-30 19:34:11 -0300

    added more fetching for tracks and albums

--- app/script.js
+++ app/script.js
@@ -15,14 +15,37 @@ const USER2 = `Extixc`;
 // Calls data for the first user from the Last.fm API and stores it on the site
 
 let U1_TopArtists;
+let U1_TopTracks;
 
-    
-fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
-.then(response => response.json())
-.then(data => {
-  U1_TopArtists = data.topartists.artist;
-  console.log("Fetched Data:", U1_TopArtists)
-})
+function U1_getData() {
+
+  // Top Artists
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
+    .then(response => response.json())
+    .then(data => {
+      U1_TopArtists = data.topartists.artist;
+      console.log("Fetched Data:", U1_TopArtists)
+    })
+
+  // Top Albums
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopAlbums&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
+    .then(response => response.json())
+    .then(data => {
+      U1_TopArtists = data.topalbum.album;
+      console.log("Fetched Data:", U1_TopAlbums)
+    })
+
+  // Top Tracks
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
+    .then(response => response.json())
+    .then(data => {
+      U1_TopArtists = data.toptracks.track;
+      console.log("Fetched Data:", U1_TopTracks)
+    })
+
+}
+
+U1_getData()
 
 document.querySelector(".button").addEventListener("click", function(e) {
   e.preventDefault()
