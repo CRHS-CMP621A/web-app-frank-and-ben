d3312e9ae05310593c1cbfacfc00d67c457dbb38
Alchium <alchiumial@gmail.com>
2023-05-30 14:46:33 -0300

    Css started

--- app/script.js
+++ app/script.js
@@ -1,18 +1,40 @@
-let KEY = 'b725d7c3f1302cf77740c3198f72ff35';
-let USER = 'Superfrancis123';
+let KEY = `b725d7c3f1302cf77740c3198f72ff35`;
+let USER1 = `Superfrancis123`;
+let USER2 = `Exticx`;
+
+//// USER 1 DATA ////
+// Calls data for the first user from the last.fm API and stores it on the site
+
+// Top Stats //
 
 // Top Artists 
-fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=${USER}&api_key=${KEY}&format=json`)
+fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
   .then(response => response.json())
   .then(data => {
-    const topArtists = data.topartists.artist;
+    const U1topArtists = data.topartists.artist;
 
-    console.log(topArtists);
-    console.log(`Top Artists for ${USER}:`);
-    console.log(topArtists[0].name)
-    console.log(topArtists[1].name)
-    console.log(topArtists[2].name)
-    console.log(topArtists[3].name)
-    console.log(topArtists[4].name)
+    console.log(U1topArtists)
+
+  })
+
+// Top Tracks
+fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
+  .then(response => response.json())
+  .then(data => {
+    const U1topTracks = data.toptracks.track;
+
+    console.log(U1topTracks)
+
+  })
+
+// Recent Stats //
+
+// Recent Tracks
+fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&limit=5&user=${USER1}&api_key=${KEY}&format=json`)
+  .then(response => response.json())
+  .then(data => {
+    const U1recentTracks = data.recenttracks.track;
+
+    console.log(U1recentTracks)
 
   })
